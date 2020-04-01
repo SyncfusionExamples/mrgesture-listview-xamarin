6c6ce9890d3c1d043cbdce9f0ba96db6f55edd8d
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-01 19:01:25 +0530

    XAMARIN-34034 - Sample added

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public partial class MainPage : MR.Gestures.ContentPage
+    {
+        #region Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
